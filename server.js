8c1f60607c366e417a6d480b8668cf3ae2e55256
Zathras <ChampionMike42@gmail.com>
2021-09-05 23:27:41 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// Entry point for the application
+import express from "express";
+import morgan from "morgan";
+import config from "./config.js";
+import apiRouter from "./router.js"
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("Hello World");
+});
+
+app.use(morgan("dev"));
+
+
+app.use(express.json());
+
+app.use("/api", apiRouter);
+
+app.listen(config.port, () => {
+  console.log(`Server 🏃🏾‍♂️ at: http://localhost:${config.port}`);
+});
