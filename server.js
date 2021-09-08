a0e2d9268c434454fe3224277dc7d2e06756d434
Zathras <ChampionMike42@gmail.com>
2021-09-08 11:00:11 -0500

    Finalize changes, server connects

--- server.js
+++ server.js
@@ -2,20 +2,20 @@
 import express from "express";
 import morgan from "morgan";
 import config from "./config.js";
-import apiRouter from "./router.js"
+// import apiRouter from "./router.js"
 
 const app = express();
 
-app.get("/", (_, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (_, res) => {
+//   res.send("Hello World");
+// });
 
 app.use(morgan("dev"));
 
-
+app.use(express.static("public"));
 app.use(express.json());
 
-app.use("/api", apiRouter);
+// app.use("/api", apiRouter);
 
 app.listen(config.port, () => {
   console.log(`Server 🏃🏾‍♂️ at: http://localhost:${config.port}`);
