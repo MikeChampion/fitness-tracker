0f1fb83c43e3b5ce145dba24405a71d6e2f3db6f
Zathras <ChampionMike42@gmail.com>
2021-09-15 11:55:35 -0500

    Cleaning up code

--- public/api.js
+++ public/api.js
@@ -10,6 +10,7 @@ const API = {
 
     return json[json.length - 1];
   },
+  
   async addExercise(data) {
     const id = location.search.split("=")[1];
 
@@ -18,27 +19,23 @@ const API = {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
     });
-
     const json = await res.json();
-
     return json;
   },
+
   async createWorkout(data = {}) {
     const res = await fetch("/api/workouts", {
       method: "POST",
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" }
     });
-
     const json = await res.json();
-
     return json;
   },
 
   async getWorkoutsInRange() {
     const res = await fetch(`/api/workouts/range`);
     const json = await res.json();
-
     return json;
   },
 };
