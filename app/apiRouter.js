3b4657bd2b711b31fc07f832454dd072affe62d6
Zathras <ChampionMike42@gmail.com>
2021-09-15 11:56:31 -0500

    Update apiRouter.js

--- app/apiRouter.js
+++ app/apiRouter.js
@@ -3,79 +3,52 @@ import fitnessController from "./controller.js";
 
 const router = new Router();
 
-router.get("/workouts", async (req, res) => {
+router.get("/workouts", async (_, res) => {
+  try {
+    const workout = await fitnessController.index();
+    res.json(workout);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post("/workouts", async (_, res) => {
     try {
-        const workouts = await fitnessController.getAll();
-        res.json(workouts);
+      const newWorkout = await fitnessController.create();
+      res.status(201).json(newWorkout);
     } catch (error) {
-        res.status(500).send({message: err.message});
+      res.status(400).json(error.message);
     }
 });
 
-router.get("/stats", async (req, res) => {
+router.get("/workouts/range", async (_, res) => {
     try {
-        const workouts = await fitnessController.getAll();
+        const workouts = await fitnessController.show();
         res.json(workouts);
-    } catch (error) {
-        res.status(500).send({message: err.message});
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 });
 
-router.get("/workouts/range", async (req, res) => {
-    try {
-        const workouts = await fitnessController.getAll();
-        while (workouts.length > 7) {
-            workouts.shift();
-        };
-        res.json(workouts);
-    } catch (error) {
-        res.status(500).send({message: err.message});
-    }
+router.put("/workouts/:id", async (req, res) => {
+  try {
+    const updatedWorkout = await fitnessController.update(
+      req.params.id,
+      req.body
+    );
+    res.status(200).send(updatedWorkout);
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
 });
 
-// router.get("/notes/:id", async (req, res) => {
-//   try {
-//     const note = await noteController.show(req.params.id);
-//     res.status(201).json(note);
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// });
-
-// router.put("notes/:id", async (req, res) => {
-//   try {
-//     const updateNote = await noteController.update(req.params.id, req.body);
-//     res.status(200).json(updateNote);
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// });
-
-// router.post("/notes", async (req, res) => {
-//   try {
-//     const newNote = await noteController.create(req.body);
-//     res.status(201).json(newNote);
-//   } catch (error) {
-//     res.status(400).json(error.message);
-//   }
-// });
-
-// router.delete("/notes/:id", async (req, res) => {
-//   try {
-//     await noteController.delete(req.params.id);
-//     res.status(204).send();
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// });
-
-// router.delete("/notes", async (req, res) => {
-//   try {
-//     await noteController.deleteAll();
-//     res.status(204).send();
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// });
+router.delete("/workouts/", async (req, res) => {
+  try {
+    await fitnessController.deleteAll();
+    res.status(204).send();
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
+});
 
 export default router;
