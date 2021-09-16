3ccac55dad4b2c2e82dc211736648691c5eab66e
Zathras <ChampionMike42@gmail.com>
2021-09-16 18:10:13 -0500

    Update env  var

--- app/config.js
+++ app/config.js
@@ -4,5 +4,5 @@ dotenv.config();
 
 export default {
   port: process.env.PORT || 3000,
-  db: process.env.DB_CLIENT_URL || "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000",
+  db: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000",
 };
