cc7dbe4dc9e77bb6b8b51736f5f57666f6774308
Zathras <ChampionMike42@gmail.com>
2021-09-16 18:07:46 -0500

    Add env var for heroku

--- app/config.js
+++ app/config.js
@@ -4,5 +4,5 @@ dotenv.config();
 
 export default {
   port: process.env.PORT || 3000,
-  db: process.env.DB_CLIENT_URL,
+  db: process.env.DB_CLIENT_URL || "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000",
 };
